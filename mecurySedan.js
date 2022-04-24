bf6f878594698f7a50905064b99b202694e4468c
tjs6595 <t.schultejans@schenckprocess.com>
2022-04-23 19:30:35 -0500

    Array Automotive submittal without bonus.

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,5 +1,45 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+const vehicleModule = require("./vehicle")
+
+class Car extends vehicleModule.Vehicle(){
+    constructor(maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService){
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    loadPassenger = function(num){
+        if (Car.passenger < Car.maximumPassengers){
+            return availableRoom = true;
+        }
+        else{
+            return availableRoom = false;
+        }
+    }
+
+    start = function(){
+        if (Car.fuel > 0){
+            return super.start = true;
+        }
+        else{
+            return super.start = false;
+        }
+    }
+
+    scheduleService = function(mileage){
+        if (mileage > 30000){
+            return Car.scheduleService = true;
+        }
+        else{
+            return Car.scheduleService = false;
+        }
+    }
+
+}
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
